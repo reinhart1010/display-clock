bae8b84d882450bda5e410ea42bfafb7d89d08e2
Reinhart Previano K <reinhart_previano@yahoo.com>
2018-06-22 22:19:49 +0700

    Make weather calls a function

--- js/weather.js
+++ js/weather.js
@@ -1,20 +1,23 @@
 /* Define variables from cookies */
 var weatherapikeyc = getCookie("com.reinhart.display.weather.weatherapikey");
 var weathercityidc = getCookie("com.reinhart.display.weather.weathercityid");
-var weatherrefreshtimec = getCookie("com.reinhart.display.weather.weatherrefreshtime");
+var weatherrefreshtimec = getCookie("com.reinhart.display.weather.weatherrefreshtime") * 60 * 1000;
 
 /* Request JSON file from OpenWeatherMap API */
-var req = new XMLHttpRequest();
-req.onreadystatechange = function(){
-  if (this.readyState == 4 && this.status == 200){
-    var jsonCom = JSON.parse(this.responseText);
-    ParseWeather(jsonCom);
-  }
+function UpdateWeather(){
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+      var jsonData = JSON.parse(this.responseText);
+      ParseWeather(jsonData);
+    }
+  };
+  req.open("GET", "api.openweathermap.org/data/2.5/weather?id=" + weathercityidc + "&units=" + weatherunitsc, true);
+  //req.open("GET", "assets/testscript/openweathermap-apitest-1.json", true); //Sample API
+  req.overrideMimeType("application/json");
+  req.send(null);
+  setTimeout(GetWeather, weatherrefreshtimec);
 };
-//req.open("GET", "api.openweathermap.org/data/2.5/weather?id=" + weathercityidc + "&units=" + weatherunitsc, true);
-req.open("GET", "assets/testscript/openweathermap-apitest-1.json", true); //Sample API
-req.overrideMimeType("application/json");
-req.send(null);
 
 /* Parse current weather to clock */
 function ParseWeather(json){
@@ -23,13 +26,4 @@ function ParseWeather(json){
   document.getElementById('mintemp').innerHTML = json.main.temp_min.toFixed(0);;
   document.getElementById('maxtemp').innerHTML = json.main.temp_max.toFixed(0);;
   document.getElementById('weathericon').className = "owf owf-" + json.weather[0].id + "-" + json.weather[0].icon.slice(-1);
-};
-
-/* Refresh weather */
-function UpdateWeather(){
-
-};
-
-/* Save weather settings to cookie */
-function SaveWeatherSettings(){
 };

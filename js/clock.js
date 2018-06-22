3f244f34f4b646f1cc490dfcfe8aab7b575e9723
Reinhart Previano K <reinhart_previano@yahoo.com>
2018-06-22 23:57:03 +0700

    Add clock customization support, part 1

--- js/clock.js
+++ js/clock.js
@@ -1,24 +1,50 @@
 /* Define variables from cookies */
 var foregroundcolorc = getCookie("com.reinhart.display.clock.foregroundcolor");
 var backgroundcolorc = getCookie("com.reinhart.display.clock.backgroundcolor");
+var timeformatc = getCookie("com.reinhart.display.timeformat");
+
+/* Define variables from IDs */
+var dateid = document.getElementById('date');
+var largeclockid = document.getElementById('largeclock');
+var amtextid = document.getElementById('amtext');
+var secondclockid = document.getElementById('secondclock');
+var clockbodyid = document.getElementById('clockbody');
+
+/* Determine time format */
+function GetTimeFormat(){
+};
 
 /* Update clock values */
 function UpdateClock(){
-  var colondeterminer = moment().second() % 2;
-  document.getElementById('date').innerHTML = moment().format('dddd, MMMM Do YYYY');
-  document.getElementById('largeclock').innerHTML = moment().format('hh:mm');
-  document.getElementById('amtext').innerHTML = moment().format('A')
-  document.getElementById('secondclock').innerHTML = moment().format('ss');
-
+  dateid.innerHTML = moment().format('dddd, MMMM Do YYYY');
+  secondclockid.innerHTML = moment().format('ss');
+  if(timeformatc = "12h"){
+    largeclockid.innerHTML = moment().format('hh:mm');
+    amtextid.innerHTML = moment().format('A');
+  } else if(timeformatc = "24h"){
+    largeclockid.innerHTML = moment().format('HH:mm');
+    amtextid.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;";
+  };
   UpdateCurrentTime();
+
   setTimeout(UpdateClock, 500);
 };
 
-function LoadClockSettings(){
-  var clockbody = document.getElementById('clockbody');
-  clockbody.style.color = foregroundcolorc;
-  clockbody.style.backgroundColor = backgroundcolorc;
+/* Apply clock settings */
+function ApplyClockSettings(){
+  clockbodyid.style.color = foregroundcolorc;
+  clockbodyid.style.backgroundColor = backgroundcolorc;
 };
+
+/* Load clock settings */
+function LoadClockSettings(){
+  var weatherapikeyc = getCookie("com.reinhart.display.weather.weatherapikey");
+  var weathercityidc = getCookie("com.reinhart.display.weather.weathercityid");
+  var weatherrefreshtimec = getCookie("com.reinhart.display.weather.weatherrefreshtime") * 60 * 1000;
+  var defaultunitsc = getCookie("com.reinhart.display.defaultunits")
+};
+
+/* Save clock settings */
 function SaveClockSettings(){
   var foregroundcolor = document.getElementById('foregroundcolor').value;
   var backgroundcolor = document.getElementById('backgroundcolor').value;
@@ -29,10 +55,10 @@ function SaveClockSettings(){
   var defaultunits = document.getElementById('defaultunits').value;
   setCookie("com.reinhart.display.clock.foregroundcolor",foregroundcolor,730);
   setCookie("com.reinhart.display.clock.backgroundcolor",backgroundcolor,730);
-  setCookie("com.reinhart.display.timeformat",backgroundcolor,730);
+  setCookie("com.reinhart.display.timeformat",timeformat,730);
   setCookie("com.reinhart.display.weather.weatherapikey",weatherapikey,730);
   setCookie("com.reinhart.display.weather.weathercityid",weathercityid,730);
   setCookie("com.reinhart.display.weather.weatherrefreshtime",weatherrefreshtime,730);
   setCookie("com.reinhart.display.defaultunits",defaultunits,730);
-  //LoadClockSettings();
+  ApplyClockSettings();
 };

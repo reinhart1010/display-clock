6c3c8e89538361175d50b0eccc41995b4ddaa1e1
Reinhart Previano K <reinhart_previano@yahoo.com>
2018-06-23 10:53:56 +0700

    Change how GetCookie() is done

--- js/settings.js
+++ js/settings.js
@@ -8,17 +8,7 @@ function setCookie(cname, cvalue, exdays) {
 
 /* Main function to call cookies */
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+  var re = new RegExp(cname + "=([^;]+)");
+  var value = re.exec(document.cookie);
+  return (value != null) ? unescape(value[1]) : null;
 };
